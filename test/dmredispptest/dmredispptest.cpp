d739bf98fc60ff3f041b70aef1cf52ab63cf1642
brinkqiang <brink.qiang@gmail.com>
2022-11-06 21:25:49 +0800

    add

--- test/dmredispptest/dmredispptest.cpp
+++ test/dmredispptest/dmredispptest.cpp
@@ -24,29 +24,15 @@
 
 #include <iostream>
 
-#ifdef _WIN32
-#include <Winsock2.h>
-#endif /* _WIN32 */
 #include "dmevent/dmevent_module.h"
 #include "dmrand.h"
-
+#include "dmsocketinit.h"
 // 注意client生命周期
 
 cpp_redis::client client;
 
 int
 main(void) {
-#ifdef _WIN32
-    //! Windows netword DLL init
-    WORD version = MAKEWORD(2, 2);
-    WSADATA data;
-
-    if (WSAStartup(version, &data) != 0) {
-        std::cerr << "WSAStartup() failure" << std::endl;
-        return -1;
-    }
-#endif /* _WIN32 */
-
     DMEVENT_INIT();
     DMEVENT_BEGIN
     {
@@ -152,10 +138,6 @@ main(void) {
 
     // synchronous commit, timeout
     // client.sync_commit(std::chrono::milliseconds(100));
-
-#ifdef _WIN32
-    WSACleanup();
-#endif /* _WIN32 */
 
     return 0;
 }
